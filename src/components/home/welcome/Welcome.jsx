e643e5887499f3ea565964931f1befc91886daf1
RossWebFS <shimon.vip2006@gmail.com>
2025-01-01 17:25:21 +0200

    Added home screen ui

---
+++ src/components/home/welcome/Welcome.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
+
+import styles from "./welcome.style";
+import { SIZES, icons } from "../../../constants";
+
+const jobTypes = ["Full-time", "Part-time", "Contractor"];
+
+const Welcome = () => {
+  const router = useRouter();
+  const [activeJobType, setactiveJobType] = useState(jobTypes[0]);
+
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Adrian</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            value=""
+            onChange={() => {}}
+            placeholder="What are you looking for?"
+            style={styles.searchInput}
+          />
+        </View>
+
+        <TouchableOpacity style={styles.searchBtn} onClick={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMethod="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setactiveJobType(item);
+                router.push(`/search/${item}`);
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{columnGap: SIZES.small}}
+          horizontal
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
